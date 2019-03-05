83c34e42eca755e88d03c321cf8627e7b6217aa6
omfgtora <spaminol@gmail.com>
2019-03-04 16:58:27 -0800

    Fixed routes. Root route goes to home_controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
+  # resource :home, only: [:index, :login], :path => 'anythingelse'
+  root 'home#index'
+  get '/login', to: 'home#login'
+  get '/signup', to: 'characters#new'
   resource :spells, only: [:index, :show]
   resource :wands, only: [:new, :edit, :update]
   resource :characters, only: [:show, :new, :create, :edit, :update]
   resource :houses, only: [:index, :show]
-
-  get '/', to: 'application#index'
-  get '/signup', to:  'characters#new'
-  get '/login', to:  'application#login'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
