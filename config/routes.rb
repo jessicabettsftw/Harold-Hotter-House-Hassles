6ab9449718d3b891c59ab000f043ce3d6d4309e6
omfgtora <spaminol@gmail.com>
2019-03-04 22:02:40 -0800

    Styled Signup form. Fixed routes (resources -> resource). Pluralized view folders.

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   root 'home#index'
   get '/login', to: 'home#login'
   get '/signup', to: 'characters#new'
-  resources :spells, only: [:index, :show]
-  resources :wands, only: [:new, :edit, :update]
-  resources :characters, only: [:show, :create, :edit, :update]
-  resources :houses, only: [:index, :show]
+  resource :spells, only: [:index, :show]
+  resource :wands, only: [:new, :edit, :update]
+  resource :characters, only: [:show, :create, :edit, :update]
+  resource :houses, only: [:index, :show]
 end
