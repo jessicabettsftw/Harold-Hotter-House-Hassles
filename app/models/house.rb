91fa2fd8664eb06a85cd1fffa4f5cbb7a84fb1d7
github username <github email address>
2019-03-05 20:59:58 -0800

    display House#index page, House#show page, added display method to House model

--- app/models/house.rb
+++ app/models/house.rb
@@ -1,2 +1,14 @@
 class House < ApplicationRecord
+
+  def display_values
+    length = self.values.length
+    values = self.values[1..(length - 2)]
+    values = values.gsub('"', '')
+  end
+
+  def display_colors
+    length = self.colors.length
+    colors = self.colors[1..(length - 2)]
+    colors = colors.gsub('"', '')
+  end
 end
