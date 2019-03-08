619babdb57f953225c92221bbc2d4065ebb58ecb
omfgtora <spaminol@gmail.com>
2019-03-08 02:01:33 -0800

    Added points to character#show. Removed depricated #getFamilars

--- app/models/character.rb
+++ app/models/character.rb
@@ -10,17 +10,6 @@ class Character < ApplicationRecord
 
   validates :name, presence: true, uniqueness: true, length: {minimum: 3, maximum: 16}, format: {with: /\A[a-z ]+(?: [a-z ]+)?\z/i}
   validates :password, presence: true, length: {minimum: 3, maximum: 16}
-  # ________________
-  # getFamiliars
-  # ________________
-  # displays a list of familiar animals to use
-  def getFamiliars
-    familiarData = {}
-    File.open("db/seedFiles/familiarData.rb") do |file|
-      familiarData = eval(file.read)
-    end
-    familiarData[:Species]
-  end
 
   def calculate_points
     count = 0
